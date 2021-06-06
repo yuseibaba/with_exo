41dbf6ed755c0d3558ea16599ccad38293615bf6
Ubuntu <ubuntu@ip-172-31-44-38.ap-northeast-1.compute.internal>
2021-06-06 13:24:10 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     #Prefix Verb   URI Pattern               Controller#Action
+     #root GET    /                         toppages#top
+    #about GET    /about(.:format)          toppages#about
+   #signup GET    /signup(.:format)         users#new
+    #users GET    /users(.:format)          users#index
+          #POST   /users(.:format)          users#create
+#edit_user GET    /users/:id/edit(.:format) users#edit
+     #user GET    /users/:id(.:format)      users#show
+          #PATCH  /users/:id(.:format)      users#update
+          #PUT    /users/:id(.:format)      users#update
+          #DELETE /users/:id(.:format)      users#destroy
+          
   root to: 'toppages#top'
-  
   get 'about', to: 'toppages#about'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
+  
 end
