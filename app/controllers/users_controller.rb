f90b5362c9248ea64b3cbd5fe90aed4c6b8afe9e
Ubuntu <ubuntu@ip-172-31-44-38.ap-northeast-1.compute.internal>
2021-06-10 11:14:01 +0000

    modify before _action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :edit, :update]
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(10)
